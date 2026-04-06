61623410abe5942715eabc39c968ecdd92eb9151
Aerex0 <suyash.ranjan.min24@itbhu.ac.in>
2026-04-07 02:45:14 +0530

    Back on react with context-api

---
+++ context-API/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './components/login'
+import Profile from './components/profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1 className='text-3xl font-bold underline'>Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ context-API/src/components/login.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/userContext";
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {setUser} = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({username, password});
+    }
+
+    return(
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
